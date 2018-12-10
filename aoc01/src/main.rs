3519fd7e6664ae16df24eaf7fd52b291ace15f1c
Paul Sajna <sajattack@gmail.com>
2018-12-10 02:36:54 -0800

    Day 1 complete

---
+++ aoc01/src/main.rs
@@ -0,0 +1,49 @@
+mod utils;
+
+use crate::utils::*;
+
+fn main() {
+    let input: String = get_input(1).expect("Failed to retrieve input");
+    //part1(input);
+    part2(input);
+}
+
+fn part1(input: String) {
+    let lines = input.split("\n");
+    let mut sum = 0;
+    for line in lines {
+        let operand = &line.parse::<i32>();
+        match operand {
+            Ok(num) => {sum += num;}
+            Err(_) => {},
+        }
+    }
+    println!("Answer: {}", sum);
+    println!("{}", submit_answer(1, 1, format!("{}", sum))
+        .expect("Failed to submit answer"));
+}
+
+fn part2(input: String) { 
+    use std::collections::HashSet;
+    let mut frequencies: HashSet<i32> = HashSet::new();
+    frequencies.insert(0);
+    let addends: Vec<i32> = input.split("\n").filter_map(|i| i.parse::<i32>().ok()).collect();
+    let mut sum = 0;
+    let mut answer = 0;
+    let mut twice: bool = false; 
+    while !twice {
+        for addend in &addends {
+            sum += addend;
+            twice = frequencies.contains(&sum);
+            frequencies.insert(sum);
+            if twice {
+                answer = sum; 
+                break;
+            }
+        }
+    }
+    println!("Answer: {}", answer);
+    println!("{}", submit_answer(1, 2, format!("{}", answer))
+        .expect("Failed to submit answer"));
+
+}
