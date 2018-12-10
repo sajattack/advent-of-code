0e088e416bf72c6cd7a0bd3d91cff94884449a3f
Paul Sajna <sajattack@gmail.com>
2018-12-10 06:22:54 -0800

    Day 2

---
+++ aoc02/src/main.rs
@@ -0,0 +1,56 @@
+#[path = "../../utils.rs"]
+mod utils;
+
+use crate::utils::*;
+
+fn main() {
+    let input: String = get_input(2).expect("Failed to retrieve input");
+    //part1(&input);
+    part2(&input);
+}
+
+fn part1(input: &String) {
+    let lines = input.split("\n");
+    let mut twos = 0;
+    let mut threes = 0;
+    for line in lines {
+        let mut two_found = false;
+        let mut three_found = false;
+        for ch in "abcdefghijklmnopqrstuvwxyz".chars() {
+            if line.matches(ch).collect::<Vec<&str>>().len() == 2 && !two_found {
+                twos += 1;
+                two_found = true;
+            } 
+            if line.matches(ch).collect::<Vec<&str>>().len() == 3 && !three_found {
+                threes += 1;
+                three_found = true;
+            }
+        }
+    }
+    let answer = twos * threes;
+    println!("{}", answer);
+    println!("{}", submit_answer(2, 1, format!("{}", answer))
+        .expect("Failed to submit answer"));
+}
+
+fn part2(input: &String) { 
+    use difference::Changeset;
+    let lines = input.split("\n").collect::<Vec<&str>>();
+    'outer: for line_a in &lines {
+        for line_b in &lines {
+            let changeset = Changeset::new(line_a, line_b, "");
+            if changeset.distance == 2 {
+            let mut answer = format!("{}", format!("{:?}", changeset.diffs[0])
+                .replace("Same", "")
+                .trim_matches(|c| c == '(' || c == ')' || c == '"'));
+            answer += format!("{:?}", changeset.diffs[3])
+                .replace("Same", "")
+                .trim_matches(|c| c == '(' || c == ')' || c == '"');
+            println!("{}", answer);
+            println!("{}", submit_answer(2, 2, format!("{}", answer))
+                .expect("Failed to submit answer"));
+            break 'outer;
+            } 
+        }
+    }
+}
---
+++ utils.rs
@@ -0,0 +1,55 @@
+extern crate reqwest;
+extern crate html2text;
+
+use std::env;
+use std::collections::HashMap;
+use std::io::BufReader;
+use std::result;
+use std::boxed::Box;
+use std::error::Error;
+
+use reqwest::header::COOKIE;
+
+// Error macro shamelessly stolen from BurntSushi
+macro_rules! err {
+    ($($tt:tt)*) => { Err(Box::<Error>::from(format!($($tt)*))) }
+}
+
+type Result<T> = result::Result<T, Box<Error>>;
+
+fn get_cookie() -> Result<String> {
+    let args: Vec<String> = env::args().collect();
+    let cookie: String; 
+    if args.len() != 2 {
+        return err!("You must specify your adventofcode session cookie as the first and only argument")
+    } else {
+       cookie = args[1].clone(); 
+    }
+    Ok(cookie)
+}
+
+pub fn get_input(day: u8) -> Result<String> {
+    let cookie = get_cookie().unwrap();
+    let client = reqwest::Client::new();
+    let input = client.get(
+        &format!("https://adventofcode.com/2018/day/{}/input", day))
+        .header(COOKIE, format!("session={}", cookie).as_str())
+        .send()?.text()?;
+    Ok(input)
+}
+
+pub fn submit_answer(day: u8, level: u8, answer: String) -> Result<String> {
+    let cookie = get_cookie().unwrap();
+    let client = reqwest::Client::new();
+    let mut data = HashMap::new();
+    let level_string = format!("{}", level);
+    data.insert("level", level_string.as_str());
+    data.insert("answer", answer.as_str());
+    let html = client.post(
+        &format!("https://adventofcode.com/2018/day/{}/answer", day))
+        .header(COOKIE, format!("session={}", cookie).as_str())
+        .form(&data)
+        .send()?.text()?;
+    let plaintext = html2text::from_read(BufReader::new(html.as_bytes()), 80);
+    Ok(plaintext)
+}
